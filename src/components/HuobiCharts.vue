bc02a7c6d40cabf3d2abd6ee56ef318a073e7ea9
aLIEzsss4 <vuetomato@gmail.com>
2019-02-24 23:05:53 +0800

    show

---
+++ src/components/HuobiCharts.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page">
+        <h3>引用了子集  {{tit}}</h3>
+
+        <h4>{{txt}}</h4>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name:'huobiCharts',
+  props:{
+    txt:String
+  },
+
+  data() {
+    return {
+        tit:'子集标题是  ABC'
+    }
+  },
+  
+  
+ 
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
---
+++ src/views/Huobi.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page">
+      <h2>虚拟货币实时行情</h2>
+
+      <HChars txt="htjs"/>
+  </div>
+</template>
+
+
+
+<script type="text/ecmascript-6">
+
+import HChars from '@/components/HuobiCharts.vue'
+
+export default {
+  data() {
+    return {
+        dataObj:{
+
+        }
+    }
+  },
+  components: {
+    HChars
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
